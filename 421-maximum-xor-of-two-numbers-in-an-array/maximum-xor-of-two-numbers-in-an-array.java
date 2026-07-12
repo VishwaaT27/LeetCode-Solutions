ba1f0edd6b19bb1801c73cde2f1da6fee36c6d7f
VishwaaT27 <vishwaat27@gmail.com>
2026-07-13 02:27:01 +0530

    Time: 761 ms (60.36%) | Memory: 208.9 MB (42.77%) - LeetSync

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
@@ -0,0 +1,52 @@
+class Solution 
+{
+
+    static class TrieNode {
+        TrieNode[] child = new TrieNode[2];
+    }
+
+    public int findMaximumXOR(int[] nums) {
+
+        TrieNode root = new TrieNode();
+
+        // Insert all numbers into Trie
+        for (int num : nums) {
+            TrieNode curr = root;
+
+            for (int i = 31; i >= 0; i--) {
+                int bit = (num >> i) & 1;
+
+                if (curr.child[bit] == null)
+                    curr.child[bit] = new TrieNode();
+
+                curr = curr.child[bit];
+            }
+        }
+
+        int maxXor = 0;
+
+        // Find best XOR partner for each number
+        for (int num : nums) {
+
+            TrieNode curr = root;
+            int xor = 0;
+
+            for (int i = 31; i >= 0; i--) {
+
+                int bit = (num >> i) & 1;
+                int opposite = 1 - bit;
+
+                if (curr.child[opposite] != null) {
+                    xor |= (1 << i);
+                    curr = curr.child[opposite];
+                } else {
+                    curr = curr.child[bit];
+                }
+            }
+
+            maxXor = Math.max(maxXor, xor);
+        }
+
+        return maxXor;
+    }
+}
