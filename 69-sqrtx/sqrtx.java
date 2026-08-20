896c71b566c2ae8830d91c5c90cecb1976fa2f32
VishwaaT27 <vishwaat27@gmail.com>
2026-08-20 10:14:35 +0530

    Refactor mySqrt method for improved accuracy

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,14 +1,14 @@
-class Solution {
-    public int mySqrt(int x) {
-        int l = 0, r = x;
-        while (l < r) {
-            int mid = (l + r + 1) >>> 1;
-            if (mid > x / mid) {
-                r = mid - 1;
-            } else {
-                l = mid;
-            }
-        }
-        return l;
+int mySqrt(int x) 
+{
+    if (x < 2) return x;
+    int left = 2,right = x / 2;
+    while (left <= right) 
+    {
+        int pivot = left + (right - left) / 2;
+        long num = (long) pivot * pivot;
+        if (num > x) right = pivot - 1;
+        else if (num < x) left = pivot + 1;
+        else return pivot;
     }
+    return right;
 }
