66872dd36746089cf4d93388c612f32baec32188
VishwaaT27 <vishwaat27@gmail.com>
2026-08-20 10:30:45 +0530

    Update find-median-from-data-stream.java

--- 295-find-median-from-data-stream/find-median-from-data-stream.java
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,26 +1,20 @@
-class MedianFinder {
-    private PriorityQueue<Integer> minQ = new PriorityQueue<>();
-    private PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
-
-    public MedianFinder() {
+class MedianFinder 
+{
+    private PriorityQueue<Integer> small;
+    private PriorityQueue<Integer> large;
+    public MedianFinder() 
+    {
+        small = new PriorityQueue<>(Collections.reverseOrder());
+        large = new PriorityQueue<>();
     }
-
-    public void addNum(int num) {
-        maxQ.offer(num);
-        minQ.offer(maxQ.poll());
-        if (minQ.size() - maxQ.size() > 1) {
-            maxQ.offer(minQ.poll());
-        }
+    public void addNum(int num) 
+    {
+        small.offer(num);
+        large.offer(small.poll());
+        if (small.size() < large.size()) small.offer(large.poll());
     }
-
     public double findMedian() {
-        return minQ.size() == maxQ.size() ? (minQ.peek() + maxQ.peek()) / 2.0 : minQ.peek();
+        if (small.size() > large.size()) return small.peek();
+        return (small.peek() + large.peek()) / 2.0;
     }
 }
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder obj = new MedianFinder();
- * obj.addNum(num);
- * double param_2 = obj.findMedian();
- */
